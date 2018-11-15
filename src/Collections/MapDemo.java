efd30c5f12e200abd816b83cedcc1851346b7be6
Tousif <md.tousifmemon@gmail.com>
2018-11-16 00:52:28 +0530

    Commit4

--- src/Collections/MapDemo.java
+++ src/Collections/MapDemo.java
@@ -3,6 +3,7 @@ package Collections;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -113,12 +114,14 @@ public class MapDemo {
 		System.out.println("PRINTING VALUES USING ITERATOR :");
 		
 		Iterator<Integer> itr1 = values.iterator();
-
+		
+		int countEven = 0;
 		while(itr1.hasNext()) {
 			int v = itr1.next();
 			if(	v%2==0 )
-				System.out.println(v);
+				countEven++;
 		}
+		System.out.println("countEven : "+countEven);
 		
 //		while(itr1.hasNext()) {
 //			if(	(itr1.next())%2==0 )
@@ -127,5 +130,53 @@ public class MapDemo {
 		
 
 		System.out.println("hm.get() : "+hm.get('U'));
+		
+		
+		
+		
+		
+		System.out.println("\n\tPRINTING PAIRS :\n");
+	
+		int[] ar = {10, 20, 20, 10, 10, 30, 50, 10, 20};
+		
+        Set<Integer> set = new HashSet<>();
+        int pairs = 0;
+
+        for (int i = 0; i < ar.length; i++) {
+            if (set.contains(ar[i])) {
+                pairs++;
+                set.remove(ar[i]);
+            } else {
+                set.add(ar[i]);
+            }
+        }
+        System.out.println("pair = :"+pairs);
+		
+		/*Map<Integer, Integer> hm1 = new HashMap<>();
+
+		for(int temp : ar) {
+			
+			if(hm1.containsKey(temp)) {
+				hm1.put(temp, hm1.get(temp)+1);
+			} 
+			else {
+				hm1.put(temp, 1);
+			}
+		}
+		
+		System.out.println(hm1);
+		
+		Collection<Integer> val = hm1.values();
+		
+		Iterator<Integer> itr2 = val.iterator();
+		int pairs=0;
+
+		while(itr2.hasNext()) {
+			int i = (int)(itr2.next());
+			pair = pair + i/2;
+		}
+		
+		System.out.println("pair = :"+pair);
+*/
 	}
 }
