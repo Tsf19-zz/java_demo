6aeb3aaa63d13ad895b8836ba3fbed2c3ab9418a
Tousif <md.tousifmemon@gmail.com>
2019-04-14 20:49:44 +0530

    PersonSorting Java8Way Added

--- src/Java8/JavaBrains/_2_PersonSorting/Java8Way_1_/App.java
+++ src/Java8/JavaBrains/_2_PersonSorting/Java8Way_1_/App.java
@@ -1,11 +1,10 @@
 /**
  * 
  */
-package Java8.JavaBrains._2_PersonSorting.Java7Way_2_;
+package Java8.JavaBrains._2_PersonSorting.Java8Way_1_;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -20,26 +19,16 @@ public class App {
 		}
 
 	}
-	
-/**
-	private static void printLastNameBeginningWithC(List<Person> persons) {
-		for( Person person : persons) {
-			if(person.getLastName().toLowerCase().startsWith("c"))
-				System.out.println(person);
-		}		
-	} 
-*/
-	//Instead of "printLastNameBeginningWithC", we make "printLastNameBeginningConditionally"
-	private static void printLastNameBeginningConditionally(List<Person> persons, Condition condition) {
+
+	private static void printConditionally(List<Person> persons, Condition condition) {
 		for( Person person : persons) {
 			if(condition.test(person))
 				System.out.println(person);
 		}		
 	}
-	
+
 	public static void main(String[] args) {
 		List<Person> persons = Arrays.asList(
-
 				new Person("firstName1","alastName1",11),
 				new Person("firstName2","cblastName2",22),
 				new Person("firstName3","calastName3",33),
@@ -48,12 +37,15 @@ public class App {
 				);
 
 		// Step 1: Sort list by last name
-		Collections.sort(persons, new Comparator<Person>() {
-			@Override
-			public int compare(Person o1, Person o2) {
-				return o1.getLastName().compareToIgnoreCase(o2.getLastName());
-			}
-		});
+//		Collections.sort(persons, new Comparator<Person>() {
+//			@Override
+//			public int compare(Person o1, Person o2) {
+//				return o1.getLastName().compareToIgnoreCase(o2.getLastName());
+//			}
+//		});
+		Collections.sort(persons,
+				(Person p1, Person p2) -> p1.getLastName().compareToIgnoreCase(p2.getLastName())
+				);
 
 		// Step 2: Print all elements in the list
 		System.out.println("\nPrint all elements in the list sorted by last name ");
@@ -61,12 +53,16 @@ public class App {
 
 		// Step 3: Print all persons that have last name begin with 'C'
 		System.out.println("\nPrint all persons that have last name begin with 'C' ");
+		/**
 		printLastNameBeginningConditionally(persons, new Condition() {
 			@Override
 			public Boolean test(Person person) {
 				return person.getLastName().toLowerCase().startsWith("c");
 			}
 		});
+		 */
+
+		printConditionally(persons, person -> person.getLastName().toLowerCase().startsWith("c") );
 
 	}
 
