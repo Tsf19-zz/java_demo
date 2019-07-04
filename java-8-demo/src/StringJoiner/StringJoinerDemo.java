958e5591a56f10d96d96897fcd21e33ba2ebd0ba
md.tousif <DOMAIN\md.tousif@tousif-optiplex-3020.blr.cordis>
2019-07-04 14:22:10 +0530

    // Joining arbitrary number of String

---
+++ java-8-demo/src/StringJoiner/StringJoinerDemo.java
@@ -0,0 +1,25 @@
+package StringJoiner;
+
+import java.util.Arrays;
+
+public class StringJoinerDemo {
+
+	public static void main(String[] args) {
+
+		// Joining arbitrary number of String
+		String combined = String.join(" and ", "LinkedIn", "Microsoft");
+		System.out.println("string joined by and : " + combined);
+
+		// joining elements from a list of String
+		String joined = String.join("|", Arrays.asList("Java", "Android", "Oracle"));
+		System.out.println("String joined by pipe from list : " + joined);
+
+		// joining String elements of an array
+		String[] biggies = { "Apple", "Google", "Amazon" };
+		String fromArray = String.join(",", biggies);
+		System.out.println("String joined by comma from array: " + fromArray);
+
+
+	}
+
+}
