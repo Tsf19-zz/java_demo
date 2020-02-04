12e525c4ab30ba1c85b13010cbcee66704ab39f5
md.tousif <DOMAIN\md.tousif@tousif-optiplex-3020.blr.cordis>
2020-02-04 18:42:52 +0530

    updated

--- src/CoreJavaTopics/Miscellaneous.java
+++ src/CoreJavaTopics/Miscellaneous.java
@@ -15,6 +15,10 @@ public class Miscellaneous {
 		System.out.println(j.equals(BigInteger.ZERO)); //false
 		System.out.println(j == BigInteger.ZERO.intValue()); //true
 	
+		j = 10;
+		System.out.println(j == null); //true
+		System.out.println(j.equals(null)); //true
+		
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
 		
 		List<Integer> subList1 = list.subList(0, 5);
--- src/CoreJavaTopics/StringManipulation.java
+++ src/CoreJavaTopics/StringManipulation.java
@@ -10,56 +10,76 @@ import java.util.regex.Pattern;
 
 public class StringManipulation {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		String str1 = " This  is ; a  paragraph  It contains .words,  .and.    sentences, and.separators.";
-		String str2, str3, str4, str5, str6, str7;
+		String temp1 = "";
+		String temp2 = "";
+		String[] tempArr = null;
 		
-		System.out.println("str1 :"+str1);
+		
+		/** ACTUAL RAW STRING */
+		System.out.println("SOP1 :"+str1);
 		//-This--is-;-a--paragraph--It-contains-.words,--.and.----sentences,-and.separators.
 		
+		/** CONVERT TO LOWER CASE */
 		str1 = str1.toLowerCase();
-		System.out.println("str1 :"+str1);
+		System.out.println("SOP2 :"+str1);
 		//-this--is-;-a--paragraph--it-contains-.words,--.and.----sentences,-and.separators.
 		
+		/** REPLACING . ; , */
 		str1 = str1.replaceAll("\\.|\\;|\\," , " ");
-		System.out.println("str1 :"+str1);
+		System.out.println("SOP3 :"+str1);
 		//-this--is---a--paragraph--it-contains--words----and-----sentences--and-separators-
 		
-		str2 = str1.replaceAll("\\s +" , " ");
-		System.out.println("str2 :"+str2);
-		//-this-is-a-paragraph-it-contains-words-and-sentences-and-separators-
-		
+		/** REMOVING EXTRA SPACES */
 		
 //		To eliminate spaces at the beginning and at the end of the String, use String.trim() method
-
-		str3 = str1.trim().replaceAll("\\s +" , " ");
-		System.out.println("str3 :"+str3+" <--CORRECT");
+		String str2 = str1.trim().replaceAll("\\s +" , " ");
+		str2 = str1.trim().replaceAll(" +" , " ");
+		str2 = str1.trim().replaceAll("( )+" , " ");
+		System.out.println("SOP4 :"+str2);
 		//this-is-a-paragraph-it-contains-words-and-sentences-and-separators
 		
 		
-		str4 = str1.trim().replaceAll("( )+" , " "); //SAME OUTPUT
-		System.out.println("str4 :"+str4+" <--CORRECT");
+		/** REMOVING EXTRA SPACES WITHOUT TRIM() */
+
+//		^_+		:	any sequence of spaces at the beginning of the string
+//		_+$		:	any sequence of spaces at the end of the string
+//		(_)+	:	any sequence of spaces that matches none of the above, meaning it's in the middle
+//		$1		:	The replacement string $1 means "group 1" (the first group made by a set of brackets).
 		
-		
-		//WITHOUT TRIM()
-		
-//		^_+ : any sequence of spaces at the beginning of the string
-//		_+$ : any sequence of spaces at the end of the string
-//		(_)+ : any sequence of spaces that matches none of the above, meaning it's in the middle
-		
-		str5 = str1.replaceAll("^ +| +$|( )+" , " ");
-		
-		System.out.println("str5 :"+str5);
+		String str3 = str1.replaceAll("^ +| +$|( )+" , " ");
+		System.out.println("SOP5 :"+str3);
 		//-this-is-a-paragraph-it-contains-words-and-sentences-and-separators-
 		
-		str6 = str1.replaceAll("^ +| +$|( )+" , "$1");
-		System.out.println("str6 :"+str6+" <--CORRECT");
+		str3 = str1.replaceAll("^ +| +$|( )+" , "$1");
+		System.out.println("SOP6 :"+str3+" <--CORRECT");
 		//this-is-a-paragraph-it-contains-words-and-sentences-and-separators
 	
 	
-		str7 = str1.trim().replaceAll("\\s{2,}" , " ");
-		System.out.println("str7 :"+str7+" <--CORRECT");
+		str3 = str1.trim().replaceAll("\\s{2,}" , " ");
+		System.out.println("SOP7 :"+str3+" <--CORRECT");
+		
+		/**To extract the first digits of a String and return it as a string, return empty string if it doesn't start with digits.*/
+		String str4 = "01Aa1b2c3";
+		temp1 = str4.replaceAll("^(\\d*).*","$1");
+		temp2 = str4.replaceAll("\\D.*", "");
+		//This regex matches from the first non-digit encountered to the end and just deletes it (replaces with nothing).
+		System.out.println();
+		
+//		A character class such as [abc] means "match one character that is either a OR b or c.
+//		Note that it doesn't mean "match one character that is a AND b AND c, that would not make any sense.
+
+//		A negated character class such as [^abc] means "match one character that is neither a nor b nor c,
+//		in other words it is not a AND not b AND not c.
+//		Notice that number 1 is all about OR, and that number 2 is all about AND.
+
+//		Therefore [^\d\s] means "match one character that is not a digit AND not a whitespace", while
+//		[\D\S] means "match one character that is either a non-digit OR a non-whitespace"
 		
 		//https://www.tutorialspoint.com/html/html_url_encoding.htm
 		try {
@@ -78,6 +98,12 @@ public class StringManipulation {
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
+		
+		
+		
+		//split from digit and return a,b & c
+		String str6 = "a1b2c3";
+		String sarr[] = str6.split("\\d");
 		
 		String str8 = "I [need] this ]message () without (this[])";
 		str8 = str8.replaceAll("[\\[\\]\\(\\)]", "");
@@ -143,6 +169,52 @@ public class StringManipulation {
 		if(s.startsWith("{") && s.endsWith("}")) {
 			System.out.println("TRUE");
 		}
+		
+//		postfixExpressionArrayList.contains("and")
+//		&& !postfixExpressionArrayList.contains("AND") && !postfixExpressionArrayList.contains("&&");
+		String andOr = " F1 and F2 F3  F4 F5 ";
+		if(andOr.matches("(.*)(and|AND|&&)(.*)"))
+			System.out.println("true");
+		else 
+			System.out.println("false");
+		
+		
+//		if(filterValue.contains("-") || filterValue.contains("#") || filterValue.contains(" "))
+		String temp = "F1F2F3F4";
+		if(temp.matches("(.*)(-|#|&&| )(.*)"))
+			System.out.println("true");
+		else 
+			System.out.println("false");
+
+		temp = "F1F2F3F4";
+		if(temp.matches("(.*)(-|#|&&| )(.*)"))
+			System.out.println("true");
+		else 
+			System.out.println("false");
+		
+		
+		temp = "-*a1b(c2d)h_e3f-g4h";
+		if(temp.matches("(.*)[^a-zA-Z0-9](.*)"))
+			System.out.println("true");
+		else 
+			System.out.println("false");
+
+		temp = "a1bc2dh_e3fg4h";
+		temp = "groceries|";
+		if(temp.matches("(.*)[^\\w'](.*)"))
+			System.out.println("true");
+		else 
+			System.out.println("false");
+		
+		//split underscore also
+		tempArr = temp.split("[^a-zA-Z0-9]");
+		
+		// \w won't split underscore
+		tempArr = temp.split("[^\\w']+");
+		
+		// \W = ^\w
+		tempArr = temp.split("[\\W']+");
+		System.out.println();
 		
 	}
 
